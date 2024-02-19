0ad66cd3b073f63f886a930bcb947c2a7733e869
breadbread1984 <breadbread1984@163.com>
2024-02-19 16:18:18 +0800

    update

--- train.py
+++ train.py
@@ -5,12 +5,13 @@ from os import mkdir
 from os.path import join, exists
 import tensorflow as tf
 from create_datasets import Dataset
-from models import Predictor
+from models import GatedGraphNeuralNetwork
 
 FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('dataset', default = None, help = 'path to directory containing tfrecord files')
+  flags.DEFINE_integer('batch', default = 32, help = 'batch size')
   flags.DEFINE_string('ckpt', default = 'ckpt', help = 'path to checkpoint')
   flags.DEFINE_integer('epoch', default = 200, help = 'epoch number')
   flags.DEFINE_integer('channels', default = 256, help = 'output channel of gated graph neural network')
@@ -21,51 +22,20 @@ def add_options():
 
 def main(unused_argv):
   parse_func = Dataset().get_parse_function()
-  trainset = tf.data.TFRecordDataset(join(FLAGS.dataset, 'trainset.tfrecord')).map(parse_func).prefetch(10).shuffle(10).batch(1)
-  testset = tf.data.TFRecordDataset(join(FLAGS.dataset, 'testset.tfrecord')).map(parse_func).prefetch(10).shuffle(10).batch(1)
+  trainset = tf.data.TFRecordDataset(join(FLAGS.dataset, 'trainset.tfrecord')).map(parse_func).prefetch(10).shuffle(10).batch(FLAGS.batch)
+  valset = tf.data.TFRecordDataset(join(FLAGS.dataset, 'testset.tfrecord')).map(parse_func).prefetch(10).shuffle(10).batch(FLAGS.batch)
 
   optimizer = tf.keras.optimizers.Adam(tf.keras.optimizers.schedules.CosineDecayRestarts(FLAGS.lr, first_decay_steps = FLAGS.decay_steps))
-  predictor = Predictor(channels = FLAGS.channels, num_layers = FLAGS.layers)
-  mae = tf.keras.losses.MeanAbsoluteError()
-
-  if not exists(FLAGS.ckpt): mkdir(FLAGS.ckpt)
-  checkpoint = tf.train.Checkpoint(model = predictor, optimizer = optimizer)
-  checkpoint.restore(tf.train.latest_checkpoint(join(FLAGS.ckpt, 'ckpt')))
-
-  log = tf.summary.create_file_writer(FLAGS.ckpt)
-
-  for epoch in range(FLAGS.epoch):
-    # train
-    train_metric = tf.keras.metrics.Mean(name = 'loss')
-    train_iter = iter(trainset)
-    for (adjacent, atoms), label in train_iter:
-      with tf.GradientTape() as tape:
-        pred = predictor(adjacent, atoms)
-        loss = mae(label, pred)
-      train_metric.update_state(loss)
-      grads = tape.gradient(loss, predictor.trainable_variables)
-      optimizer.apply_gradients(zip(grads, predictor.trainable_variables))
-      print('Step: #%d epoch: %d loss: %f lr: %f' % (optimizer.iterations, epoch, train_metric.result(), optimizer.lr))
-      if optimizer.iterations % FLAGS.save_freq == 0:
-        checkpoint.save(join(FLAGS.ckpt, 'ckpt'))
-      with log.as_default():
-        tf.summary.scalar('loss', train_metric.result(), step = optimizer.iterations)
-    # evaluation
-    eval_metric = tf.keras.metrics.MeanAbsoluteError()
-    eval_iter = iter(testset)
-    preds = list()
-    gts = list()
-    for (adjacent, atoms), label in eval_iter:
-      pred = predictor(adjacent, atoms)
-      gts.append(label)
-      preds.append(pred)
-    preds = tf.concat(preds, axis = 0)
-    gts = tf.concat(gts, axis = 0)
-    eval_metric.update_state(gts, preds)
-    with log.as_default():
-      tf.summary.scalar('mae', eval_metric.result(), step = optimizer.iterations)
-    print('Step: #%d epoch: %d mae: %f' % (optimizer.iterations, epoch, eval_metric.result()))
-  checkpoint.save(join(FLAGS.ckpt, 'ckpt'))
+  model = GatedGraphNeuralNetwork(channels = FLAGS.channels, num_layers = FLAGS.layers)
+  loss = [tf.keras.losses.MeanAbsoluteError()]
+  metrics = [tf.keras.metrics.MeanAbsoluteError()]
+  model.compile(optimizer = optimizer, loss = loss, metrics = metrics)
+  if exists(FLAGS.ckpt): model.load_weight(join(FLAGS.ckpt, 'ckpt', 'variables', 'variables'))
+  callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir = FLAGS.ckpt),
+    tf.keras.callbacks.ModelCheckpoint(filepath = join(FLAGS.ckpt, 'ckpt'), save_freq = FLAGS.save_freq, save_best_only = True, mode = "min")
+  ]
+  model.fit(trainset, epochs = FLAGS.epoch, validation_data = valset, callbacks = callbacks)
 
 if __name__ == "__main__":
   add_options()
